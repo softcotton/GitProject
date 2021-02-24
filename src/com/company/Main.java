0a6b95197fa9d1ca1a2ecbeab4cdd855f8c56218
Welson <welson@uw.edu>
2021-02-23 18:51:08 -0800

    These are my second changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Git!");
         var person = new Person();
+        System.out.println(person);
 	// write your code here
     }
 }
